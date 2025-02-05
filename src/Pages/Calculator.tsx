aa23c5bb48c40150603913a14959883be757fa58
Ben Fletcher <Ben.Fletcher@enable.com>
2025-02-05 14:51:43 +0000

    Update turn

--- src/Pages/Calculator.tsx
+++ src/Pages/Calculator.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import './Calculator.css';
-import { GameEvent } from '../models/turn';
+import { BallColour } from '../models/turn';
 import useGameStore from '../store/store';
 
 const Calculator = () => {
@@ -11,10 +10,10 @@ const Calculator = () => {
         <div className="calculator-container">
             <div style={{ borderRadius: '1px solid black', width: "100vw", height: "80%" }}></div>
             <div className="button-container">
-                <Button className="calc-button red-button" onClick={() => submitEvent(GameEvent.PottedRed)}>Red</Button>
-                <Button className="calc-button black-button" onClick={() => submitEvent(GameEvent.PottedBlack)}>Black</Button>
-                <Button className="calc-button foul-button" onClick={() => submitEvent(GameEvent.FoulRed)}>Foul</Button>
-                <Button className="calc-button miss-button" onClick={() => submitEvent(GameEvent.Miss)}>Miss</Button>
+                <Button className="calc-button red-button" onClick={() => submitEvent({color: BallColour.Red, potted: true})}>Red</Button>
+                <Button className="calc-button black-button" onClick={() => submitEvent({color: BallColour.Black, potted: true})}>Black</Button>
+                <Button className="calc-button foul-button" onClick={() => submitEvent({color: BallColour.Na, potted: false, foul: true})}>Foul</Button>
+                <Button className="calc-button miss-button" onClick={() => submitEvent({color: BallColour.Na, potted: false})}>Miss</Button>
             </div>
         </div>
     );
