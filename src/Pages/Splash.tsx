72e09efc00babbc1f5aa595b9a6dcb9fdf610bb8
Sasha Kataev <sasha.kataev@enable.com>
2025-02-05 14:56:24 +0000

    Start game

--- src/Pages/Splash.tsx
+++ src/Pages/Splash.tsx
@@ -1,14 +1,29 @@
 import { useState } from 'react';
 import { Button, Form, ListGroup, Container, Row, Col } from 'react-bootstrap';
+import useGameStore from '../store/store';
+import { useNavigate } from 'react-router';
 
 export default function Splash() {
     const [name, setName] = useState('');
     const [names, setNames] = useState<string[]>([]);
+    const addPlayer = useGameStore((state) => state.addPlayer);
+    const navigate = useNavigate();
 
     const addName = () => {
-        if (name.trim() !== '') {
+        if (name.trim() !== '' && !names.includes(name)) {
             setNames([...names, name]);
             setName('');
+        } else if (names.includes(name)) {
+            alert('Name already exists');
+        }
+    };
+
+    const startGame = () => {
+        if (names.length > 0) {
+            names.forEach((name) => addPlayer({ name, turns: [] }));
+            navigate('/calculator');
+        } else {
+            alert('Cannot start game with 0 players');
         }
     };
 
@@ -17,7 +32,10 @@ export default function Splash() {
     };
 
     return (
-        <Container className="d-flex flex-column justify-content-start align-items-center" style={{ height: '100vh', marginTop: '100px', maxWidth: '450px' }}>
+        <Container
+            className="d-flex flex-column justify-content-start align-items-center position-relative"
+            style={{ height: '100vh', maxWidth: '450px', paddingTop: '100px' }}
+        >
             <Row className="w-100">
                 <Col xs={12} className="d-flex flex-column align-items-center">
                     <h1 style={{ marginBottom: '40px', fontSize: '36px', fontWeight: 'bold' }}>Pooker Score</h1>
@@ -33,7 +51,7 @@ export default function Splash() {
                     <Button onClick={addName} variant="primary" className="mb-3" style={{ width: '150px', height: '50px', fontSize: '20px' }}>
                         Add
                     </Button>
-                    <ListGroup className="w-100" style={{ maxHeight: '200px', overflowY: 'auto' }}>
+                    <ListGroup className="w-100" style={{ maxHeight: '425px', overflowY: 'auto', marginBottom: '80px' }}>
                         {names.map((name, index) => (
                             <ListGroup.Item key={index} className="d-flex justify-content-between align-items-center" style={{ backgroundColor: '#e9ecef' }}>
                                 {name}
@@ -43,6 +61,15 @@ export default function Splash() {
                     </ListGroup>
                 </Col>
             </Row>
+            <Button
+                onClick={startGame}
+                variant="success"
+                className="position-fixed"
+                style={{ width: '150px', height: '50px', fontSize: '20px', bottom: '20px' }}
+                disabled={names.length === 0}
+            >
+                Start Game
+            </Button>
         </Container>
     );
 }
