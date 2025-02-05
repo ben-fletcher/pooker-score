04f47ecf920a9db24470f491a1e05c9965959caf
Callum Gill <callum.gill@enable.com>
2025-02-05 14:39:27 +0000

    Initial scoreboard models

--- src/models/player.ts
+++ src/models/player.ts
@@ -4,3 +4,16 @@ export type Player = {
   name: string;
   turns: Turn[];
 };
+
+export type Turn = {
+  score: number;
+  event: GameEvent;
+  ballIndex: number;
+};
+
+export enum GameEvent {
+  NULL,
+  NON_BLACK,
+  BLACK,
+  FOUL,
+}
