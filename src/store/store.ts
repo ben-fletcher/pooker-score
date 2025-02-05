67f28987b45a6fac824c3a1b6fb2aae54af7c5e2
Callum Gill <callum.gill@enable.com>
2025-02-05 16:05:48 +0000

    Initial scoreboard logic (#3)
    * Initial scoreboard logic
    
    * Remove dummy data

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,25 +1,34 @@
 import { create } from "zustand";
 import { Player } from "../models/player";
-import { GameEvent } from "../models/turn";
+import { BallTurn, GameEvent } from "../models/turn";
 
 interface GameState {
-  activePlayer: Player | null;
+  activePlayer: number;
   players: Player[];
+  currentBallTurn: BallTurn;
+  previousBallTurns: BallTurn[];
   addPlayer: (player: Player) => void;
   submitEvent: (event: GameEvent) => void;
   clearPlayers: () => void;
 }
 
+const getDefaultBallTurn = () => ({
+  ballIndex: 0,
+  playerEvents: [],
+});
+
 const useGameStore = create<GameState>((set) => ({
-  activePlayer: null,
+  activePlayer: 0,
   players: [],
+  currentBall: 0,
+  currentBallTurn: getDefaultBallTurn(),
+  previousBallTurns: [],
   addPlayer: (player: Player) =>
     set((state) => ({ players: [...state.players, player] })),
   submitEvent: (event: GameEvent) => {
     console.log(event);
   },
-  clearPlayers: () =>
-    set((state) => ({ ...state, players: [] })),
+  clearPlayers: () => set((state) => ({ ...state, players: [] })),
 }));
 
 export default useGameStore;
